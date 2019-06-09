ed08713f79d073fd4701e318a6770a36197074c5
dongzhixiong <dongzhixiong001@ke.com>
2019-06-09 21:15:22 +0800

    skip

--- src/model.cc
+++ src/model.cc
@@ -21,7 +21,7 @@ bool TrainModel::Init(const std::string trainfile){
 float TrainModel::Sigmoid(const float& x){
    return (1 / (1 + exp(-x))); 
 }
-void TrainModel::Train(const std::vector<VocabWords>& vocab_words,const std::map<std::string,int> word_hash) {
+void TrainModel::Train(const std::vector<VocabWords>& vocab_words,const std::string& label,const std::map<std::string,int> word_hash) {
     std::ifstream in(train_file_.c_str());
     if(!in.is_open()){
         cout << "open file " <<train_file_<<" failed"<<endl;
@@ -41,36 +41,59 @@ void TrainModel::Train(const std::vector<VocabWords>& vocab_words,const std::map
         std::vector<std::string> items;
         split(items,line,boost::is_any_of(" "));
         sentence_position=0;
+        std::vector<float> w(dim,0);
+        std::vector<float> neu(dim,0);
         while(sentence_position< items.size()){
-            std::vector<float> w(dim,0);
-            std::vector<float> neu(dim,0);
             int loc;
             float g,f;
-            if((sentence_position<window)||(sentence_position+window)>items.size-1){
+            if(label=="cbow"){
+                if((sentence_position<window)||(sentence_position+window)>items.size-1){
+                    sentence_position++;
+                    continue;
+                }
+                for(int cw=sentence_position-2;cw<sentence_position+2;cw++){
+                    if(cw==sentence_position) continue;
+                    int nu=word_hash[items[cw]];
+                    VecAdd(vec0[nu],w);
+                }
+                VocabWords loc_word=vocab_words[word_hash[sentence_position]];
+                for(int d=0;d<loc_word.point.size();d++){
+                    loc=loc_word.point[d];
+                    for (int a = 0; a < dim; a++) f=w[a]*vec1[loc][a];
+                    g = (1 - loc_word.code[d] - f) * alpha;
+                    for (int b = 0; b < dim; b++) neu[c] += g * v1[loc][b];
+                    for (int c = 0; c < dim; c++) vec1[loc][c] += g * w[c];
+                }
+
+                for(int cw=sentence_position-2;cw<sentence_position+2;cw++){
+                    if(cw==sentence_position) continue;
+                    int nu=word_hash[items[cw]];
+                    VecAdd(neu,vec0[nu]);
+                }
                 sentence_position++;
-                continue;
             }
-            for(int cw=sentence_position-2;cw<sentence_position+2;cw++){
-                if(cw==sentence_position) continue;
-                int nu=word_hash[items[cw]];
-                VecAdd(vec0[nu],w);
-            }
-            VocabWords loc_word=vocab_words[word_hash[sentence_position]];
-            for(int d=0;d<loc_word.point.size();d++){
-                loc=loc_word.point[d];
-                for (int a = 0; a < dim; a++) f=w[a]*vec1[loc][a];
-                g = (1 - loc_word.code[d] - f) * alpha;
-                for (int b = 0; b < dim; b++) neu[c] += g * v1[loc][b];
-                for (int c = 0; c < dim; c++) vec1[loc][c] += g * w[c];
-            }
+            else{
+                for(int cw=sentence_position-2;cw<sentence_position+2;cw++){
+                    if(cw==sentence_position) continue;
+                    int nu=word_hash[items[cw]];
+                    VocabWords loc_word=vocab_words[nu];
+                    for(int d=0;d<loc_word.point.size();d++){
+                        loc=loc_word.point[d];
+                        for (int a = 0; a < dim; a++) f=w[a]*vec1[loc][a];
+                        g = (1 - loc_word.code[d] - f) * alpha;
+                        for (int b = 0; b < dim; b++) neu[c] += g * v1[loc][b];
+                        for (int c = 0; c < dim; c++) vec1[loc][c] += g * w[c];
+                    }
+                for(int cw=sentence_position-2;cw<sentence_position+2;cw++){
+                    if(cw==sentence_position) continue;
+                    int nu=word_hash[items[cw]];
+                    VecAdd(neu,vec0[nu]);
+                }
+                sentence_position++;
 
-            for(int cw=sentence_position-2;cw<sentence_position+2;cw++){
-                if(cw==sentence_position) continue;
-                int nu=word_hash[items[cw]];
-                VecAdd(neu,vec0[nu]);
+                }
+                 
             }
-            sentence_position++;
-
         }
          
     }
--- src/model.h
+++ src/model.h
@@ -13,7 +13,7 @@ public:
     TrainModel();
     ~TrainModel();
     bool Init(const std::string trainfile);
-    void Train(const std::vector<VocabWords>& vocab_words,const std::map<std::string,int> word_hash);
+    void Train(const std::vector<VocabWords>& vocab_words,const std::string& label,const std::map<std::string,int> word_hash);
 private:
     float Sigmoid(const float& x);
     void VecAdd(const std::vector<float>& v1,std::vector<float>& v2);
